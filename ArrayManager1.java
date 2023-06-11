538a99e588139d7a3e59e02e42459af0be51e4a9
QCT <snkta2018@gmail.com>
2023-06-11 16:35:57 +0530

    New Commit

---
+++ ArrayManager1.java
@@ -0,0 +1,20 @@
+public class ArrayManager1 
+{
+	public static void main(String[] args) 
+	{
+		int a[] = new int[5];    // Array initialization.
+		   
+	    a[0] = 10;
+	    a[1] = 20;
+	    a[2] = 130;
+	    a[3] = 130;
+	    a[4] = 130;
+		    
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+		System.out.println(a[4]);
+	}
+}
+
