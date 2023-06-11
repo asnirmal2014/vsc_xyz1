ad108dd02fd662efd24d4f196bf11ccfa65bb3d2
qcgittest2014 <32424198+asnirmal2014@users.noreply.github.com>
2023-06-11 16:52:29 +0530

    Update ArrayManager1.java
    Commit the alignment

--- ArrayManager1.java
+++ ArrayManager1.java
@@ -4,11 +4,11 @@ public class ArrayManager1
 	{
 		int a[] = new int[5];    // Array initialization.
 		   
-	    a[0] = 100;
-	    a[1] = 200;
-	    a[2] = 300;
-	    a[3] = 400;
-	    a[4] = 500;
+	    	a[0] = 100;
+	    	a[1] = 200;
+	    	a[2] = 300;
+	    	a[3] = 400;
+	    	a[4] = 500;
 		    
 		System.out.println(a[0]);
 		System.out.println(a[1]);
